87abb78d0f8d844038ec8fd118a1ababc4303360
Renan Garcia <renan.garcia@cargox.com.br>
2019-05-18 21:37:43 -0300

    Change configs SEO and Acessibility

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -14,10 +14,10 @@ import Image from "../../atoms/image";
 const Header = () => (
   <StyledHeader>
     <Toolbar>
-      <Image src={logoImage} />
+      <Image src={logoImage} alt="Mercado Libre Logo"/>
       <form>
-        <Input placeholder="Nunca dejes de buscar" />
-        <Button type="submit" />
+        <Input placeholder="Nunca dejes de buscar" aria-label="Nunca dejes de buscar"/>
+        <Button type="submit" aria-label="Buscar" />
       </form>
     </Toolbar>
   </StyledHeader>
